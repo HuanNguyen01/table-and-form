6f68f3bceee0e7ee03224f983365864de05f1460
HuanNguyen01 <huannguyen199938@gmail.com>
2024-01-23 23:19:56 +0700

    add bai 6

---
+++ bai 6/thuc-hanh/hien-thi-dau-ra.js
@@ -0,0 +1,12 @@
+
+    function ShowMessage(){
+    alert('xin chào')
+}    function ShowMessage1(){
+    document.getElementById('messageDiv').innerHTML='xin chào'
+}
+    function ShowMessage2(){
+    document.write('xin chào!!!!')
+}
+    function ShowMessage3(){
+    console.log('xin chào!!');
+}
